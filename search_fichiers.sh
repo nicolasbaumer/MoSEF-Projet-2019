abfb0a7ca9f2dd3b53b5bb179486f5ce44294d72
BaumerNicolas <nicobaumer@orange.fr>
2019-11-14 15:34:53 +0100

    ajout fonctionnalite : affichage de fichiers selon un pattern

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -2,6 +2,13 @@
 
 #Stocke la date courante dans la variable date_courante dans un format compréhensible
 date_courante=$(date +'%d %B %Y')
+
+#Message de bienvenue et affichage de la date du jour
 echo Bienvenue $USER nous sommes le $date_courante
+
+#Demande à l'utilisateur d'indiquer un répertoire et l'affiche
 read -p "Quel repertoire vous interesse ?" repertoire
 echo $repertoire
+
+#Affiche les fichiers correspondant à l'entrée de l'utilisateur qui se situent dans le répertoire demandé
+ls $repertoire/$1
